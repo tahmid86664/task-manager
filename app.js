ba5beaad8fc406de029679e448f6e3669b73b6ed
Tahmid Khandokar <tahmidkhandokar82@gmail.com>
2022-05-16 11:10:45 +0600

    basic setup of routes and mongoDB

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+const connectDB = require("./db/connect");
+const taskRoutes = require("./routes/tasks");
+
+const port = 3000;
+
+// middleware
+app.use(express.json());
+app.use("/api/v1/tasks", taskRoutes);
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
+// server will listen when there will no error in connecting mongodb
+// after successfully connection of mongoDB then, server will listen
---
+++ db/connect.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const conncetDB = (url) => {
+  return mongoose.connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+  });
+};
+
+module.exports = conncetDB;
